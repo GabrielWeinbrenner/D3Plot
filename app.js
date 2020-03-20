a50836c4c4dd865e4819a26418d7734ec2fa9b4d
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-03-20 14:05:59 -0400

    Title and axis headers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var width = 1200;
 var height = 600;
-var padding = 20; // Offsets the svgs
+var padding = 80; // Offsets the svgs
 
 
 //   var yMax = d3.max(studentScores, 
@@ -36,11 +36,13 @@ d3.select("svg")
         .attr("transform", 
             "translate(0,"+(height-padding)+")")
         .call(xAxis);
+
 var yAxis = d3.axisLeft(yScale);
 d3.select("svg")
     .append("g")
         .attr("transform", "translate("+padding+",0)")
         .call(yAxis);
+
 d3.select("svg")
     .attr("width", width)
     .attr("height", height)
@@ -56,3 +58,28 @@ d3.select("svg")
             d3.select("#currentCountry")
                 .text(d.region + " " + d.growthRate)
         })
+
+d3.select("svg")
+    .append("text")
+        .attr("x", width/2)
+        .attr("y", height-padding + 20)
+        .attr("dy", "1.5em")
+        .style("text-anchor", "middle")
+        .text("Literacy Rate")
+
+d3.select("svg")
+    .append("text")
+        .attr("x", width/2)
+        .attr("y", padding)
+        .style("text-anchor", "middle")
+        .style("font-size", "1.5em")
+        .text("Cellular Users vs Literacy Rate")
+
+d3.select("svg")
+    .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height/2)
+        .attr("y", padding - 20)
+        .attr("dy", "-1.1em")
+        .style("text-anchor", "middle")
+        .text("Cellular Users");
